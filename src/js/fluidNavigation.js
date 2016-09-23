48ba9f2c6b763a2b307b4c2f3c991e592fd6f5b5
Joshua Glazer <atrail2014@gmail.com>
2016-09-23 14:59:50 -0400

    Tag Building and anchor child node collection capabilities added. Anchor list collecting and compiling code moved from MenuConstructionEngine class to MenuModelConstructionEngine class.

--- src/js/fluidNavigation.js
+++ src/js/fluidNavigation.js
@@ -1,15 +1,21 @@
 "use strict";
 
+//settings libraray
 var fl_anchor ="a",
+    fl_menuTag = "ul",
+    fl_menuElementTag = "li",
     fl_anchorClass="fl-inline-anchors",
     fl_parentAttributeName = "fl-parent", 
     fl_childReferenceArray ="children_elements",
     fl_Regex_anchorIdSpacer = /_|-/i;
+    
 //object library
 var MenuFluidicity = function(){},
-	MenuConstructionEngine = function() {};
+	MenuModelConstructionEngine = function() {},
+	MenuConstructionEngine = function() {},
+	MenuTagBuilder = function() {};
 		
-//general function library
+//****************************************************** DEFINES: general global function library *****************************************8
 
 /**
  * determines if argument is of object type
@@ -26,10 +32,104 @@ function fl_arrayLike( array ) {
 }
 
 //object prototype definitions
+
+// ******************************************** DEFINES, ALIASES: MenuFluidicity prototype ******************************************8
 var MProto = MenuFluidicity.prototype = {
 };
-  
+
+// ******************************************** DEFINES, ALIASES: MenuTagBuilder prototype ********************************************8
+var MTSProto = MenuTagBuilder.prototype = {
+
+/**
+ * Identifies the type of object as MenuTagSpecs 
+ */
+    CLASS_NAME: "MenuTagSpecs",
+/**
+ * The tag name to be created
+ */
+    TAG_NAME: "",
+    
+/**
+ * a list of attributes for the element created where the keys are attribute names and the values are attribute values
+ */
+    TAG_PROPERTIES: [],
+    
+/**
+ * A function that builds element from the information stored in this objects properties
+ */
+    buildTag: function(document) {
+    
+        if( !"createElement" in document )
+            return false;
+        
+        var newElement = document.createElement( this.TAG_NAME );
+        
+        if( !"setAttribute" in newElement )
+            return false;
+            
+        if( !fl_arrayLike( this.TAG_PROPERTIES ) )
+            return false;
+            
+        //SHOULD ADD CODE TO HERE TO MAKE SURE keys FUNCTION IS IN TAG_PROERTIES OR CODE IN fl_arrayLike GLOBAL FUNCTION TO CHECK FOR keys
+        //FUNCTION UNIVERSALLY
+        var property_names = this.TAG_PROPERTIES.keys();
+        
+        for( var i =0; i < property_names.length; i++ ) {
+        
+            var pn = property_names[i];
+            
+            if( typeof pn == "string" )         
+                newElement.setAttribute( pn, this.TAG_PROPERTIES[ pn ] );
+        }
+        
+        return newElement;
+            
+    }    
+};
+
+//********************************************* DEFINES, ALIASES: MenuConstructionEngine prototype **********************************8
 var MCEProto = MenuConstructionEngine.prototype = {
+/**
+ * identifies object as menu construction engine type
+ */
+ 
+    CLASS_NAME: "MenuConstructionEngine",
+/**
+ * list of all anchor elements with children elements filled in
+ */
+    anchorModel: [],
+    
+/**
+ * Function that builds an array of relevant anchor elements and adds to each element a list (as an indexed array ) of child elements
+ *     takes, by dependency injection, an instance of the MenuModelConstructionEngine object as first argument and a document object or
+ *     parent element in which to search for anchors as second argument
+ */
+    construct: function( menu_model_const_engine,  document ) {
+    
+        if( !"CLASS_NAME" in menu_model_const_engine || !"getElementsByTagName" in document )
+            return false;
+        
+        if( menu_model_const_engine.CLASS_NAME != "MenuModelConstructionEngine" )
+            return false;
+            
+    //aliasing long variable name to something shorter
+        var mmmce = menu_model_const_engine;
+        
+        this.anchorModel = mmce.collectInlineAnchors( document );
+        
+        if( mmce.anchorChildrenInit( this.anchorModel ) )
+            mmce.anchorChildrenPopulate( this.anchorModel );
+        
+    }
+};
+
+// ********************************************* DEFINES, ALIASES: MenuModelConstructionEngine prototype ********************************8 
+var MMCEProto = MenuModelConstructionEngine.prototype = {
+
+/**
+ * identifies object as menu model construction engine type
+ */
+    CLASS_NAME: "MenuModelConstructionEngine",
 
 /**
  * Returns an array of all anchor ("a") elements in a document object
@@ -59,7 +159,7 @@ var MCEProto = MenuConstructionEngine.prototype = {
     getAnchorId: function( anchorElement ) {
     
         if ( !fl_isObj(anchorElement) ) 
-            return null;
+            return false;
         
         if( "getAttribute" in anchorElement )
             return anchorElement.getAttribute("id");
@@ -67,7 +167,7 @@ var MCEProto = MenuConstructionEngine.prototype = {
         if( "id" in anchorElement )
             return (typeof anchorElement.id == "string" ? anchorElement.id : null );
         
-        return null;
+        return false;
         
     },
     
@@ -115,12 +215,72 @@ var MCEProto = MenuConstructionEngine.prototype = {
  * Function to populate children arrays for each anchor element with references to all it's parent elements
  */
     anchorChildrenPopulate: function( anchorArray ) {
-    
+        
+    //verifies arguments
         if( !fl_arrayLike( anchorArray ) )
             return false;
             
-        //code to go through anchors pulling out parent title and filling subsequent children submember arraylists
+    //goes through list of valid anchors passing anchor reference to parent's child array
+        for( var l = 0; l < anchorArray.length; l++ ) {
+            
+            var a = anchorArray[l];
+            
+        //makes sure that the child reference array is present in anchor element
+            if( "push" in a[ fl_childReferenceArray ] ) {
+               
+            //stores string name of parent to current anchor
+                var parent_ = this.getAnchorParent( a );
+                
+                if( parent_ != null ) {
+                    
+                //stores a list (as an array of course, how else? ) of parent anchor element(s) for current anchor
+                    var parent_elements = this.getElementsById( anchorArray, parent_ );
+                    
+                //makes sure parent_elements is not null and is an array
+                    if( fl_arrayLike( parent_elements) ) {
+                    
+                    //passes reference of anchor element to all its parents
+                        for( var p = 0; p < parent_elements.length; p++ ) {
+                            
+                        //verifies that anchor's parent element has a child reference array
+                            if( fl_arrayLike( parent_elements[p][ fl_childReferenceArray ] )  ) {
+                            
+                            //All checks complete -> anchor element is added to parent elements child list
+                                parent_elements[p][ fl_childReferenceArray ].push( a );
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+        //as long as argument is a valid type
+        return true;
+        
+    },
+    
+/**
+ * Function to produce an array of anchor elements by a specified id value
+ */
+    getElementsById: function ( anchorArray, id ) {
+        
+        if( !fl_arrayLike( anchorArray ) || typeof id != "string" )
+            return false;
+            
+        var returnAnchors = [];
+        
+        for( var j = 0; j < anchorArray.length; j++ ) {
+        
+            if( this.getAnchorId( anchorArray[j] ) == id )
+            
+                returnAnchors.push( anchorArray[j] );
+        }
+        
+        return returnAnchors;          
+       
     }
-  
-}	
+    
+}
+// ******************************************************************************************************************************
 
